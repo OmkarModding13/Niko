2f0c456c7078b32e4ccfa347ac1feaee65a4eee4
OmkarModding13 <omkarkoli1324@gmail.com>
2026-09-09 10:57:25 +0530

    Update title in autoBan.js to use emoji syntax

--- src/events/autoBan.js
+++ src/events/autoBan.js
@@ -15,7 +15,7 @@ async function updateAutoBanEmbed(message, banCount) {
 
         const embed = new EmbedBuilder()
             .setColor('#ff0000')
-            .setTitle('🚨 Anti Bot Channel 🚨')
+            .setTitle('\u{1F6A8} Anti Bot Channel \u{1F6A8}')
 .setDescription(
     '# **DO NOT TYPE IN HERE**\n\n' +
     '**If you type in here, you will be automatically blocked from the server**\n\n' +
