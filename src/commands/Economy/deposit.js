e2f4729e1c3307ac344291d0d2b82a120c74b44d
OmkarModding13 <omkarkoli1324@gmail.com>
2026-09-20 14:26:10 +0530

    Make Inventory channel error clickable

--- src/commands/Economy/deposit.js
+++ src/commands/Economy/deposit.js
@@ -19,7 +19,7 @@ export default {
 
     execute: withErrorHandling(async (interaction, config, client) => {
         if (!isInventoryChannel(interaction.channel)) {
-            return interaction.reply({ content: '❌ Please use **/deposit** in the **『Inventory』** channel.', ephemeral: true });
+            return interaction.reply({ content: '❌ Please use **/deposit** in the <#1550120893982703616> channel.', ephemeral: true });
         }
 
         const deferred = await InteractionHelper.safeDefer(interaction);
