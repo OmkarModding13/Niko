64c69e8ed8782de0b67b22a22fb32cda3f055032
OmkarModding13 <omkarkoli1324@gmail.com>
2026-09-20 15:09:01 +0530

    Allow server owner to bypass /rank channel restriction

--- src/commands/Leveling/rank.js
+++ src/commands/Leveling/rank.js
@@ -20,7 +20,9 @@ export default {
   category: 'Leveling',
 
   async execute(interaction, config, client) {
-    if (interaction.channelId !== LEVEL_CHECK_CHANNEL_ID) {
+    const isOwner = interaction.guild?.ownerId === interaction.user.id;
+
+    if (!isOwner && interaction.channelId !== LEVEL_CHECK_CHANNEL_ID) {
       return interaction.reply({
         content: `❌ Please use **/rank** in <#${LEVEL_CHECK_CHANNEL_ID}>.`,
         ephemeral: true
